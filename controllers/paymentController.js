d68e45bfee5aeec75134e4a240bbb0ef0ad7aefd
Moksh <moksh2004jain@gmail.com>
2025-02-08 18:28:28 +0530

    checkout controller

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,11 +5,31 @@ const Payment = require('../services/db').Payment;
 const DematAccount = require('../services/db').DematAccount;
 
 exports.getCheckoutSession = catchasync(async (req, res, next) => {
-  console.log(req.body);
-  console.log('Fake payement sessoion');
+ 
+  const user = req.user.id
+  const { amount } = req.body;
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: { name: 'Wallet Top-up' },
+                        unit_amount: amount * 100, // Convert to cents
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: `http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `http://localhost:3000/cancel`,
+            metadata: { userId },
+        });
+
 
   res.status(200).json({
     status: 'success',
-    checkout_session: 'Fake Checkout Session',
+    url :  session.url,
   });
 });
